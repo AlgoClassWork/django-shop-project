e85ac7ac7f05ac869d48001a0e507ca64bafbeb5
AlgoClassWork <kyxec.play@gmail.com>
2026-05-03 11:11:18 +0600

    Add files via upload

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название') 
+    slug = models.SlugField(unique=True, verbose_name='Слаг') 
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Product(models.Model):
+    pass 
+
+class Review(models.Model):
+    pass 
+
+class Order(models.Model):
+    pass 
+
+class OrderItem(models.Model):
+    pass
