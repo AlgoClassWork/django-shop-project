427286208c82d610429335d692e074f9355b3d21
AlgoClassWork <kyxec.play@gmail.com>
2026-05-10 10:43:42 +0600

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -12,7 +12,20 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
 class Product(models.Model):
-    pass 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2 ,verbose_name='Цена')
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Изображение')
+    stock = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 class Review(models.Model):
     pass 
