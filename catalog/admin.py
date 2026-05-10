9d409fde913c14eb021bb4a42f3669a54eaeb7fb
AlgoClassWork <kyxec.play@gmail.com>
2026-05-10 10:43:54 +0600

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,3 +5,9 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
     prepopulated_fields = {'slug' : ('name', )}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'created_at')
+    list_filter = ('category', )
+    search_fields = ('name', )
