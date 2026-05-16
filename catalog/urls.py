446d8026d4e193098b6787b073f4887884d6360e
AlgoClassWork <kyxec.play@gmail.com>
2026-05-16 11:27:28 +0600

    Add files via upload

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
-
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
