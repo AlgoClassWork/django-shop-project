1c2db2638a1c7717ceccd8bf25cad6eef69d9fc6
AlgoClassWork <kyxec.play@gmail.com>
2026-05-17 11:31:06 +0600

    Add files via upload

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 
 urlpatterns = [
+    path('', product_list, name='product_list'),
+
     path('register/', register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import *
 
 def register(request):
     form = UserCreationForm()
@@ -8,3 +10,18 @@ def register(request):
         form.save()
         return redirect('login')
     return render(request, 'register.html', {'form': form})
+
+def product_list(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+
+    category_slug = request.GET.get('category')
+    if category_slug:
+        products = products.filter(category__slug=category_slug)
+
+    search_result = request.GET.get('search')
+    if search_result:
+        products = products.filter(name__icontains=search_result)
+
+    context = {'products': products, 'categories': categories}
+    return render(request, 'product_list.html', context)
